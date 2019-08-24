c5dacec014d922a4263720125f772a4544392ed6
xiamo12 <364023396@qq.com>
2019-08-24 16:01:30 +0800

    修改了配置文件

---
+++ build/precache-manifest.63e369c3c692f121982cd3a85e250045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a54bfc92639c68ef2ddcae3d105003",
+    "url": "/react_orderlist/public/index.html"
+  },
+  {
+    "revision": "95fad3ea4ee07b0d51a7",
+    "url": "/react_orderlist/public/static/css/main.3996dd04.chunk.css"
+  },
+  {
+    "revision": "6f6c0f29478d92308e66",
+    "url": "/react_orderlist/public/static/js/2.c6481815.chunk.js"
+  },
+  {
+    "revision": "95fad3ea4ee07b0d51a7",
+    "url": "/react_orderlist/public/static/js/main.633cec74.chunk.js"
+  },
+  {
+    "revision": "a4c32014563f46da913d",
+    "url": "/react_orderlist/public/static/js/runtime~main.21d6ba64.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f744dde69f924674d53d25e230556971.js"
+  "/react_orderlist/public/precache-manifest.63e369c3c692f121982cd3a85e250045.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_orderlist/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
