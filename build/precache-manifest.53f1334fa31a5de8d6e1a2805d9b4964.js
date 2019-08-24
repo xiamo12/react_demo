23ab54e4a48431575197f7f054dea0ec087d9cb6
xiamo12 <364023396@qq.com>
2019-08-24 21:48:34 +0800

    修改路径

--- build/precache-manifest.53f1334fa31a5de8d6e1a2805d9b4964.js
+++ build/precache-manifest.53f1334fa31a5de8d6e1a2805d9b4964.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "da8f9058a7b16f065839d7ec88cd5b0f",
+    "revision": "1a6a53fcd87a007d49777a8ebfad868d",
     "url": "./index.html"
   },
   {
-    "revision": "0396b023374281199508",
+    "revision": "9f2287b7c9fb7a728996",
     "url": "./static/css/main.3996dd04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.c6481815.chunk.js"
   },
   {
-    "revision": "0396b023374281199508",
-    "url": "./static/js/main.a0739a02.chunk.js"
+    "revision": "9f2287b7c9fb7a728996",
+    "url": "./static/js/main.e737d188.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
